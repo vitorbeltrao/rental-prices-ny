7523b09461f86531452f997859b4da1a5e992c0f
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-01-29 08:56:40 -0300

    💬 General clean up at upload raw data func

--- components/upload_raw_data/upload_raw_data.py
+++ components/upload_raw_data/upload_raw_data.py
@@ -1,7 +1,7 @@
 '''
 Author: Vitor Abdo
 
-This .py file serves to upload the raw data 
+This .py file serves to upload the raw data
 in W&B extracted from the data source
 '''
 
@@ -18,8 +18,10 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 # main function of this file
+
+
 def upload_raw_data(args) -> None:
-    '''Function that upload an artifact, in this 
+    '''Function that upload an artifact, in this
     case a raw dataset for weights and biases
     '''
     # start a new run at wandb
@@ -32,31 +34,29 @@ def upload_raw_data(args) -> None:
         type=args.artifact_type,
         description=args.artifact_description
     )
-    artifact.add_reference(args.input_uri) # add a HTTP link to the artifact
-    run.log_artifact(artifact) # save the artifact version to W&B and mark it as the output of this run
+    artifact.add_reference(args.input_uri)  # add a HTTP link to the artifact
+    # save the artifact version to W&B and mark it as the output of this run
+    run.log_artifact(artifact)
     logger.info('artifact uploaded to the wandb: SUCCESS')
-    return None
+
 
 if __name__ == "__main__":
     logging.info('About to start executing the upload_raw_data function')
 
     parser = argparse.ArgumentParser(
-    description='Upload an artifact to W&B. Adds a reference denoted by a URI to the artifact.'
-    )
+        description='Upload an artifact to W&B. Adds a reference denoted by a URI to the artifact.')
 
     parser.add_argument(
-        "--artifact_name", 
-        type=str, 
-        help='A human-readable name for this artifact, which is how you can identify this artifact.', 
-        required=True
-    )
+        "--artifact_name",
+        type=str,
+        help='A human-readable name for this artifact which is how you can identify this artifact.',
+        required=True)
 
     parser.add_argument(
-        '--artifact_type', 
-        type=str, 
-        help='The type of the artifact, which is used to organize and differentiate artifacts.', 
-        required=True
-    )
+        '--artifact_type',
+        type=str,
+        help='The type of the artifact, which is used to organize and differentiate artifacts.',
+        required=True)
 
     parser.add_argument(
         '--artifact_description',
@@ -67,9 +67,9 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        '--input_uri', 
-        type=str, 
-        help='Reference denoted by a URI (HTTP, for example) to the artifact.', 
+        '--input_uri',
+        type=str,
+        help='Reference denoted by a URI (HTTP, for example) to the artifact.',
         required=True
     )
 
