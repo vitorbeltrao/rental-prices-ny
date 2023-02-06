b30b9bdb1bb35e2ac92a7063f65dc922aa6c2731
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-02-06 16:42:28 -0300

    🎨 Update ML workflow

--- components/06_train_model/train_model.py
+++ components/06_train_model/train_model.py
@@ -5,11 +5,40 @@ This .py file is for training, saving the best model and
 get the feature importance for model
 '''
 
-# import necessary packages
-import logging
-import argparse
+# Import necessary packages
 import wandb
+import logging
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mlflow
+from datetime import datetime
+from scipy import stats
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import svm
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.pipeline import Pipeline, make_pipeline
+
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+from sklearn.compose import ColumnTransformer
+
+import category_encoders as ce
+
+from sklearn.impute import SimpleImputer
 
 # basic logs config
 logging.basicConfig(
